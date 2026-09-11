9467e8633f4b0f77bb65a5b43a90730366294857
SUPAKON SARNOI <6752300631@stu.pim.ac.th>
2026-09-11 16:09:06 +0700

    Deploy Vite app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ZenseviX-Company/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
