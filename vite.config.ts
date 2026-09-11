a6c8ab18c8f77fc116f3b73e96d7efddd4519487
SUPAKON SARNOI <6752300631@stu.pim.ac.th>
2026-09-11 16:22:44 +0700

    Fix static build asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ZenseviX-Company/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
